d673cb1ea68568aaf50371196c909bced9ad7cc6
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-01-18 15:24:28 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,13 @@ apt-get update -y
 apt-get install sudo md5sum postfix mpack vim portsentry ufw fail2ban apache2 mailutils -y
 
 #config sudoers
-rm -rf /etc/sudoers
 cp /root/deploy/src/sudoers /etc/
 
 #config static ip
-rm -rf /etc/network/interfaces
 cp /root/deploy/src/interfaces /etc/network/interfaces
 ifup enp0s8
 
 #config ssh
-rm -rf /etc/ssh/sshd_config
 cp /root/deploy/src/sshd_config /etc/ssh/sshd_config
 
 #config apache and web deployment
@@ -25,15 +22,12 @@ cp /root/deploy/src/sshd_config /etc/ssh/sshd_config
 pache-selfsigned.key   -out /etc/ssl/certs/apache-selfsigned.crt  -subj "/C=RU/ST=Moscow/L=Moscow\
 /O=21/OU=school/CN=192.168.56.2"
 cp /root/deploy/src/ssl-params.conf /etc/apache2/
-rm -rf /etc/apache2/sites-available/default-ssl.conf
 cp /root/deploy/src/default-ssl.conf /etc/apache2/sites-available/
-rm -rf /etc/apache2/sites-available/000-default.conf
 cp /root/deploy/src/000-default.conf /etc/apache2/sites-available/
 a2enmod ssl
 a2enmod headers
 a2ensite default-ssl
 a2enconf ssl-params
-rm -rf /var/www/html/index.html
 cp -r /root/deploy/site/* /var/www/html/
 
 #config firewall
@@ -43,25 +37,20 @@ ufw allow 80/tcp
 ufw allow 443/tcp
 
 #config fail2ban
-rm -rf /etc/fail2ban/jail.conf
 cp /root/deploy/src/jail.conf /etc/fail2ban/
 
 #config portsentry
-rm -rf /etc/default/portsentry
 cp /root/deploy/src/portsentry /etc/default/
-rm -rf /etc/portsentry/portsentry.conf
 cp /root/deploy/src/portsentry.conf /etc/portsentry/
 
 #config crontab
 cp /root/deploy/src/update_script.sh /etc/cron.d/
 cp /root/deploy/src/crontab_checker.sh /etc/cron.d/
-rm -rf /etc/crontab
 cp /root/deploy/src/crontab /etc/
 cp /root/deploy/src/crontab	/root/crontab_original
 md5sum /etc/crontab > /root/crontab_control_sum
 
 #config aliases
-rm -rf /etc/aliases
 cp /root/deploy/src/aliases /etc/
 
 #stop unusefull services
