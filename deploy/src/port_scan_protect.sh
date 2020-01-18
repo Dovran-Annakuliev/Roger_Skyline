01801572d02002e636d23496ec937d3838e6f91c
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-01-18 18:16:43 +0300

    Create port_scan_protect.sh

---
+++ deploy/src/port_scan_protect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+iptables -N port-scaning
+iptables -A port-scaning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limits 1/s --limit-burst 2 -j RETURN
+iptables -A port-scaning -j DROP
