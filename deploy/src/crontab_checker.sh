39d33905ea10fb87a90c36046db4867af0d86fd2
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-01-18 19:11:10 +0300

    Update crontab_checker.sh

--- deploy/src/crontab_checker.sh
+++ deploy/src/crontab_checker.sh
@@ -4,6 +4,6 @@ if md5sum -c /root/crontab_control_sum
 then
 	echo 'Crontab [ok]'
 else
-	diff /root/crontab_original /etc/crontab > /root/crontab_log
+	diff /root/crontab_original /var/spool/crontab/root > /root/crontab_log
 	mpack -s 'Crontab has been modified!' /root/crontab_log root@roger
 fi
